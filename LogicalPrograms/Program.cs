9bcbfe39707fd022dd1a1c634766f643746eb604
unknown <asthaagarwal203@gmail.com>
2022-02-16 18:34:36 +0530

    [Astha] Add: PrimeNo

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -12,18 +12,18 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Enter the value of n");
             int n=Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            for(int i=1;i<n;i++)
+            int count = 0;
+            for(int i=1;i<=n;i++)
             {
                 if(n%i==0)
                 {
-                    sum = sum + i;
+                    count++;
                 }
             }
-            if (sum == n)
-                Console.WriteLine("Perfect No");
+            if (count==2)
+                Console.WriteLine("Prime No");
             else
-                Console.WriteLine("Not Perfect No");
+                Console.WriteLine("Not Prime No");
         }
     }
 }
